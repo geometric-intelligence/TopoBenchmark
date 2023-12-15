8d86456c39b82099f73e0df3c1eece824103f15a
Coerulatus <marcomontagna21@gmail.com>
2023-12-15 08:51:55 +0000

    liftings work with batch now

--- topobenchmarkx/data/liftings/liftings.py
+++ topobenchmarkx/data/liftings/liftings.py
@@ -6,37 +6,49 @@ import torch_geometric
 from topobenchmarkx.data.liftings.lifting import AbstractLifting
 
 
+import copy
 class KHopLifting(torch_geometric.transforms.BaseTransform):
     def __init__(self, k=1):
         super().__init__()
         self.k = k
 
-    def forward(self, data: torch_geometric.data.Data, slices: dict = None) -> dict:
-        data_lifted = copy.copy(data)
+    def forward(self, batch: torch_geometric.data.Batch, slices: dict = None) -> dict:
+        batch_lifted = copy.copy(batch)
 
-        n_nodes = data.x.shape[0]
-
-        if slices is not None:
-            n_edges = data.edge_index.shape[1]
-            fix_idxs = torch.zeros((2, n_edges), dtype=slices["edge_index"].dtype)
-            # slices["edge_index"][-1] -= 1
-            last_idx = 0
-            for i, idx in enumerate(slices["edge_index"]):
-                if not i == 0:
-                    fix_idxs[:, last_idx:idx] = slices["x"][i - 1]
-                    last_idx = idx
-            data_lifted.edge_index += fix_idxs
+        n_nodes = batch.x.shape[0]
 
         incidence_1 = torch.zeros(n_nodes, n_nodes)
-        edge_index = torch_geometric.utils.to_undirected(data.edge_index)
+        edge_index = torch_geometric.utils.to_undirected(batch.edge_index)
         for n in range(n_nodes):
             neighbors, _, _, _ = torch_geometric.utils.k_hop_subgraph(
                 n, self.k, edge_index
             )
             incidence_1[n, neighbors] = 1
         incidence_1 = torch.Tensor(incidence_1).to_sparse_coo()
-        data_lifted["hyperedges"] = incidence_1
-        return data_lifted
+        batch_lifted["hyperedges"] = incidence_1
+        return batch_lifted
 
     def __call__(self, data, slices=None):
         return self.forward(data, slices)
+    
+class KNearestNeighborsLifting(torch_geometric.transforms.BaseTransform):
+    def __init__(self, k=1):
+        super().__init__()
+        self.transform = torch_geometric.transforms.KNNGraph(k)
+
+    def forward(self, batch: torch_geometric.data.Batch, slices: dict = None) -> dict:
+        batch_lifted = copy.copy(batch)
+        batch_lifted.pos = batch_lifted.x
+        n_nodes = batch.x.shape[0]
+        incidence_1 = torch.zeros(n_nodes, n_nodes)
+        print("ok")
+        print(batch_lifted.batch)
+        data_temp = self.transform(batch_lifted)
+        incidence_1[data_temp.edge_index[0],data_temp.edge_index[1]] = 1
+        incidence_1 = torch.Tensor(incidence_1).to_sparse_coo()
+        batch_lifted["hyperedges"] = incidence_1
+        return batch_lifted
+
+    def __call__(self, data, slices=None):
+        return self.forward(data, slices)
+    

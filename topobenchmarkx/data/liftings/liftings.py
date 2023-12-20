ca32cb037f9ad0ebc3132a412c735ac7c9ea202d
Coerulatus <marcomontagna21@gmail.com>
2023-12-20 10:46:49 +0000

    added cell lifting

--- topobenchmarkx/data/liftings/liftings.py
+++ topobenchmarkx/data/liftings/liftings.py
@@ -5,6 +5,7 @@ import numpy as np
 import torch
 import torch_geometric
 from scipy.optimize import minimize
+import networkx as nx
 
 from topobenchmarkx.data.liftings.lifting import AbstractLifting
 
@@ -60,7 +61,6 @@ class SimplicialNeighborhoodLifting(torch_geometric.transforms.BaseTransform):
             ]
 
     def forward(self, data: torch_geometric.data.Data) -> dict:
-        results = {}
         n_nodes = data.x.shape[0]
         edge_index = torch_geometric.utils.to_undirected(data.edge_index)
         simplices = [set() for _ in range(self.complex_dim + 1)]
@@ -95,9 +95,75 @@ class SimplicialNeighborhoodLifting(torch_geometric.transforms.BaseTransform):
 
         for i, field in enumerate(self.added_fields):
             if i % 3 == 0:
-                data.__setitem__(self.added_fields[0], incidences[int(i / 3)])
+                data.__setitem__(field, incidences[int(i / 3)].to_sparse_coo())
             if i % 3 == 1:
-                data.__setitem__(self.added_fields[0], laplacians_up[int(i / 3)])
+                data.__setitem__(field, laplacians_up[int(i / 3)].to_sparse_coo())
             if i % 3 == 2:
-                data.__setitem__(self.added_fields[0], laplacians_down[int(i / 3)])
+                data.__setitem__(field, laplacians_down[int(i / 3)].to_sparse_coo())
+        return data
+    
+class CellCyclesLifting(torch_geometric.transforms.BaseTransform):
+    def __init__(self, aggregation="sum"):
+        super().__init__()
+        self.added_fields = []
+        if not aggregation in ["sum"]:
+            raise NotImplementedError
+        self.aggregation = aggregation
+        self.added_fields = ["x_1", 
+                             "incidence_1", 
+                             "laplacian_down_1", 
+                             "laplacian_up_1",
+                             "incidence_2", 
+                             "laplacian_down_2", 
+                             "laplacian_up_2"]
+
+    def forward(self, data: torch_geometric.data.Data) -> dict:
+        n_nodes = data.x.shape[0]
+        # edge_index = torch_geometric.utils.to_undirected(data.edge_index)
+        edges = [(i.item(),j.item()) for i, j in zip(data.edge_index[0], data.edge_index[1])]
+        G = nx.Graph()
+        G.add_edges_from(edges)
+        cycles = nx.cycle_basis(G)
+        n_edges = len(edges)
+        n_cells = len(cycles)
+        incidence_1 = torch.zeros([n_nodes, n_edges])
+        incidence_2 = torch.zeros([n_edges, n_cells])
+        edges = [set(e) for e in edges]
+        for i, edge in enumerate(edges):
+            incidence_1[list(edge), i] = 1
+        for i, cycle in enumerate(cycles):
+            for j in range(len(cycle)):
+                if j==len(cycle)-1:
+                    edge = {cycle[j],cycle[0]}
+                else:
+                    edge = {cycle[j],cycle[j+1]}
+                incidence_2[edges.index(edge), i] = 1
+        degree = torch.diag(torch.sum(incidence_1, dim=1))
+        laplacian_up_1 = 2 * degree - torch.mm(
+            incidence_1, torch.transpose(incidence_1, 1, 0)
+        )
+        degree = torch.diag(torch.sum(incidence_2, dim=1))
+        laplacian_up_2 = 2 * degree - torch.mm(
+            incidence_2, torch.transpose(incidence_2, 1, 0)
+        )
+        degree = torch.diag(torch.sum(incidence_1, dim=0))
+        laplacian_down_1 = 2 * degree - torch.mm(
+            torch.transpose(incidence_1, 1, 0), incidence_1
+        )
+        degree = torch.diag(torch.sum(incidence_2, dim=0))
+        laplacian_down_2 = 2 * degree - torch.mm(
+            torch.transpose(incidence_2, 1, 0), incidence_2
+        )
+        
+        if self.aggregation=="sum":
+            x_1 = torch.mm(torch.transpose(incidence_1,1,0),data.x)
+                
+        data.__setitem__("incidence_1",incidence_1.to_sparse_coo())
+        data.__setitem__("incidence_2",incidence_2.to_sparse_coo())
+        data.__setitem__("laplacian_up_1",laplacian_up_1.to_sparse_coo())
+        data.__setitem__("laplacian_up_2",laplacian_up_2.to_sparse_coo())
+        data.__setitem__("laplacian_down_2",laplacian_down_2.to_sparse_coo())
+        data.__setitem__("laplacian_down_1",laplacian_down_1.to_sparse_coo())
+        data.__setitem__("x_1", x_1)
+        
         return data

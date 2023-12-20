88a903271d54a7b0c79b9d7c28289eb3ccfdc127
gbg141 <guille_gbg@hotmail.com>
2023-12-20 20:37:34 +0100

    General LiftingTransform instantiated by hydra

--- topobenchmarkx/data/liftings/lifting.py
+++ topobenchmarkx/data/liftings/lifting.py
@@ -2,18 +2,38 @@ from abc import ABC, abstractmethod
 
 import torch_geometric
 
+from topobenchmarkx.data.liftings.liftings import *
 
-class AbstractLifting(torch_geometric.transforms.BaseTransform):
-    """abstract class that provides an interface to define a custom readout"""
+LIFTINGS = {
+    # Identity
+    "IdentityLifting": IdentityLifting,
+    # Graph -> Hypergraph
+    "HypergraphKHopLifting": HypergraphKHopLifting,
+    "HypergraphKNearestNeighborsLifting": HypergraphKNearestNeighborsLifting,
+    # Graph -> Simplicial Complex
+    "SimplicialNeighborhoodLifting": SimplicialNeighborhoodLifting,
+    "SimplicialCliqueLifting": SimplicialCliqueLifting,
+    # Graph -> Cell Complex
+    "CellCyclesLifting": CellCyclesLifting,
+}
 
-    def __init__(self):
-        self.cache = {}
-        return
 
-    @abstractmethod
-    def forward(self, batch: torch_geometric.data.Batch) -> torch_geometric.data.Batch:
-        """Forward pass of the lifting
-        """
-        
+class LiftingTransform(torch_geometric.transforms.BaseTransform):
+    """abstract class that provides an interface to define a custom lifting"""
+
+    def __init__(self, lifting, **kwargs):
+        super().__init__()
+        self.lifting = LIFTINGS[lifting](**kwargs)
+
+    def forward(self, data: torch_geometric.data.Data) -> torch_geometric.data.Data:
+        """Forward pass of the lifting"""
+        lifted_data = self.lifting(data)
+        # lifted_features = self.lift_features(data) #TODO
+        return lifted_data
+
     def __call__(self, data):
-        self.forward(data)
+        return self.forward(data)
+
+
+if __name__ == "__main__":
+    _ = LiftingTransform()

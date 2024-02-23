d96fe281de3a3e028e5bbc4a72f286d630935f06
levtelyatnikov <tlscabinet@gmail.com>
2024-02-23 13:53:47 +0000

    cwn network

--- topobenchmarkx/models/wrappers/default_wrapper.py
+++ topobenchmarkx/models/wrappers/default_wrapper.py
@@ -66,3 +66,26 @@ class CANWrapper(DefaultWrapper):
         )
         model_out["x_0"] = x_0
         return model_out
+
+
+class CWNWrapper(DefaultWrapper):
+    """Abstract class that provides an interface to loss logic within network"""
+
+    def __init__(self, backbone):
+        super().__init__(backbone)
+
+    def __call__(self, batch):
+        """Define logic for forward pass"""
+        model_out = {"labels": batch.y}
+        x_0, x_1, x_2 = self.backbone(
+            x_0=batch.x_0,
+            x_1=batch.x_1,
+            x_2=batch.x_2,
+            neighborhood_0_to_1=batch.incidence_1.T,
+            neighborhood_1_to_1=batch.adjacency_1,
+            neighborhood_2_to_1=batch.incidence_2,
+        )
+        model_out["x_0"] = x_0
+        model_out["x_1"] = x_1
+        model_out["x_2"] = x_2
+        return model_out

e31c80a6e14daffc10e3eb3cabc68b789e785da0
gbg141 <guille_gbg@hotmail.com>
2024-02-28 20:45:35 +0100

    EDGNN working

--- custom_models/hypergraph/edgnn.py
+++ custom_models/hypergraph/edgnn.py
@@ -6,6 +6,156 @@ import torch_geometric
 import torch_scatter
 
 
+class MLP(nn.Module):
+    """adapted from https://github.com/CUAI/CorrectAndSmooth/blob/master/gen_models.py"""
+
+    def __init__(
+        self,
+        in_channels,
+        hidden_channels,
+        out_channels,
+        num_layers,
+        dropout=0.5,
+        Normalization="bn",
+        InputNorm=False,
+    ):
+        super(MLP, self).__init__()
+
+        self.in_channels = in_channels
+        self.hidden_channels = hidden_channels
+        self.out_channels = out_channels
+
+        self.lins = nn.ModuleList()
+        self.normalizations = nn.ModuleList()
+        self.InputNorm = InputNorm
+
+        assert Normalization in ["bn", "ln", "None"]
+        if Normalization == "bn":
+            if num_layers == 1:
+                # just linear layer i.e. logistic regression
+                if InputNorm:
+                    self.normalizations.append(nn.BatchNorm1d(in_channels))
+                else:
+                    self.normalizations.append(nn.Identity())
+                self.lins.append(nn.Linear(in_channels, out_channels))
+            else:
+                if InputNorm:
+                    self.normalizations.append(nn.BatchNorm1d(in_channels))
+                else:
+                    self.normalizations.append(nn.Identity())
+                self.lins.append(nn.Linear(in_channels, hidden_channels))
+                self.normalizations.append(nn.BatchNorm1d(hidden_channels))
+                for _ in range(num_layers - 2):
+                    self.lins.append(nn.Linear(hidden_channels, hidden_channels))
+                    self.normalizations.append(nn.BatchNorm1d(hidden_channels))
+                self.lins.append(nn.Linear(hidden_channels, out_channels))
+        elif Normalization == "ln":
+            if num_layers == 1:
+                # just linear layer i.e. logistic regression
+                if InputNorm:
+                    self.normalizations.append(nn.LayerNorm(in_channels))
+                else:
+                    self.normalizations.append(nn.Identity())
+                self.lins.append(nn.Linear(in_channels, out_channels))
+            else:
+                if InputNorm:
+                    self.normalizations.append(nn.LayerNorm(in_channels))
+                else:
+                    self.normalizations.append(nn.Identity())
+                self.lins.append(nn.Linear(in_channels, hidden_channels))
+                self.normalizations.append(nn.LayerNorm(hidden_channels))
+                for _ in range(num_layers - 2):
+                    self.lins.append(nn.Linear(hidden_channels, hidden_channels))
+                    self.normalizations.append(nn.LayerNorm(hidden_channels))
+                self.lins.append(nn.Linear(hidden_channels, out_channels))
+        else:
+            if num_layers == 1:
+                # just linear layer i.e. logistic regression
+                self.normalizations.append(nn.Identity())
+                self.lins.append(nn.Linear(in_channels, out_channels))
+            else:
+                self.normalizations.append(nn.Identity())
+                self.lins.append(nn.Linear(in_channels, hidden_channels))
+                self.normalizations.append(nn.Identity())
+                for _ in range(num_layers - 2):
+                    self.lins.append(nn.Linear(hidden_channels, hidden_channels))
+                    self.normalizations.append(nn.Identity())
+                self.lins.append(nn.Linear(hidden_channels, out_channels))
+
+        self.dropout = dropout
+
+    def reset_parameters(self):
+        for lin in self.lins:
+            lin.reset_parameters()
+        for normalization in self.normalizations:
+            if not (normalization.__class__.__name__ == "Identity"):
+                normalization.reset_parameters()
+
+    def forward(self, x):
+        x = self.normalizations[0](x)
+        for i, lin in enumerate(self.lins[:-1]):
+            x = lin(x)
+            x = F.relu(x, inplace=True)
+            x = self.normalizations[i + 1](x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.lins[-1](x)
+        return x
+
+    def flops(self, x):
+        num_samples = np.prod(x.shape[:-1])
+        flops = num_samples * self.in_channels  # first normalization
+        flops += (
+            num_samples * self.in_channels * self.hidden_channels
+        )  # first linear layer
+        flops += num_samples * self.hidden_channels  # first relu layer
+
+        # flops for each layer
+        per_layer = num_samples * self.hidden_channels * self.hidden_channels
+        per_layer += num_samples * self.hidden_channels  # relu + normalization
+        flops += per_layer * (len(self.lins) - 2)
+
+        flops += (
+            num_samples * self.out_channels * self.hidden_channels
+        )  # last linear layer
+
+        return flops
+
+
+class PlainMLP(nn.Module):
+    """adapted from https://github.com/CUAI/CorrectAndSmooth/blob/master/gen_models.py"""
+
+    def __init__(
+        self,
+        in_channels,
+        hidden_channels,
+        out_channels,
+        num_layers,
+        dropout=0.5,
+        Normalization="bn",
+        InputNorm=False,
+    ):
+        super(PlainMLP, self).__init__()
+        self.lins = nn.ModuleList()
+
+        self.lins.append(nn.Linear(in_channels, hidden_channels))
+        for _ in range(num_layers - 2):
+            self.lins.append(nn.Linear(hidden_channels, hidden_channels))
+        self.lins.append(nn.Linear(hidden_channels, out_channels))
+        self.dropout = dropout
+
+    def reset_parameters(self):
+        for lin in self.lins:
+            lin.reset_parameters()
+
+    def forward(self, x):
+        for i, lin in enumerate(self.lins[:-1]):
+            x = lin(x)
+            x = F.relu(x, inplace=True)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.lins[-1](x)
+        return x
+
+
 class EquivSetConv(nn.Module):
     def __init__(
         self,

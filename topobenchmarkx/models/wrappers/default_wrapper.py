fixed scn normalization

topobenchmarkx/models/wrappers/default_wrapper.py
@@ -65,22 +65,41 @@ class SANWrapper(DefaultWrapper):
 
 
 # TODO: relocate this function to a more appropriate location
+# def normalize_matrix(matrix):
+#     import numpy as np
+#     import scipy as sp
+#     from topomodelx.utils.sparse import from_sparse
+
+#     matrix = matrix.to_dense().numpy()
+#     m, n = matrix.shape
+#     diags_ = abs(matrix).sum(axis=1)
+
+#     with np.errstate(divide="ignore"):
+#         diags_sqrt = 1.0 / np.sqrt(diags_)
+#     diags_sqrt[np.isinf(diags_sqrt)] = 0
+#     diags_sqrt = sp.sparse.csr_array(
+#         sp.sparse.spdiags(diags_sqrt.T, 0, m, n, format="csr")
+#     )
+#     return from_sparse(sp.sparse.csr_matrix(diags_sqrt @ (matrix @ diags_sqrt)))
+ 
+
+# TODO: finish proper normalization
 def normalize_matrix(matrix):
-    import numpy as np
-    import scipy as sp
-    from topomodelx.utils.sparse import from_sparse
+    
+    matrix = matrix.to_dense()
+    n,_ = matrix.shape
+    abs_matrix = abs(matrix)
+    diag_sum = abs_matrix.sum(axis=1)
+    
+    # Handle division by zero
+    idxs = torch.where(diag_sum != 0)
+    diag_sum[idxs] = 1.0 / torch.sqrt(diag_sum[idxs])
 
-    matrix = matrix.to_dense().numpy()
-    m, n = matrix.shape
-    diags_ = abs(matrix).sum(axis=1)
-
-    with np.errstate(divide="ignore"):
-        diags_sqrt = 1.0 / np.sqrt(diags_)
-    diags_sqrt[np.isinf(diags_sqrt)] = 0
-    diags_sqrt = sp.sparse.csr_array(
-        sp.sparse.spdiags(diags_sqrt.T, 0, m, n, format="csr")
-    )
-    return from_sparse(sp.sparse.csr_matrix(diags_sqrt @ (matrix @ diags_sqrt)))
+    
+    diag_indices = torch.stack([torch.arange(n), torch.arange(n)])
+    diag_matrix = torch.sparse_coo_tensor(diag_indices, diag_sum, matrix.shape, device=matrix.device).coalesce()
+    normalized_matrix = diag_matrix @ (matrix @ diag_matrix)
+    return torch.sparse_coo_tensor(normalized_matrix.nonzero().T, normalized_matrix[normalized_matrix != 0], (n,n))
 
 
 class SCNWrapper(DefaultWrapper):
58871718cdc6d2011f5c0c14fda32d2f938fab34
levtelyatnikov <tlscabinet@gmail.com>
2024-05-18 21:55:08 +0200

    correcting sh files

--- hp_scripts/main_exp/hypergraph/unignn2.sh
+++ hp_scripts/main_exp/hypergraph/unignn2.sh
@@ -123,8 +123,8 @@ do
     model.feature_encoder.out_channels=32,64,128 \
     model.backbone.n_layers=1,2,3,4 \
     model.feature_encoder.proj_dropout=0.25,0.5 \
-    dataset.parameters.data_seed=0,3,5 \
-    dataset.parameters.batch_size=128,256 \
+    dataset.parameters.data_seed=0,3,5,7,9 \
+    dataset.parameters.batch_size=1 \
     logger.wandb.project=TopoBenchmarkX_Hypergraph \
     trainer.max_epochs=1000 \
     trainer.min_epochs=50 \

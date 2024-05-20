2351fb7b66abba2424f131fc91d53811bb55f23b
Guille <guille_gbg@hotmail.com>
2024-05-20 16:43:03 +0000

    Preeliminar env setup

--- env.sh
+++ env.sh
@@ -3,17 +3,18 @@
 pip install --upgrade pip
 pip install -e '.[all]'
 
-pip install git+https://github.com/pyt-team/TopoNetX.git
-pip install git+https://github.com/pyt-team/TopoModelX.git
-pip install git+https://github.com/pyt-team/TopoEmbedX.git
+pip install --no-dependencies git+https://github.com/pyt-team/TopoNetX.git
+pip install --no-dependencies git+https://github.com/pyt-team/TopoModelX.git
+pip install --no-dependencies git+https://github.com/pyt-team/TopoEmbedX.git
 
-CUDA="cu121" # if available, select the CUDA version suitable for your system
-             # e.g. cpu, cu102, cu111, cu113, cu115
-pip install fsspec==2024.5.0
-pip install torch==2.0.1 --extra-index-url https://download.pytorch.org/whl/${CUDA}
-pip install torch-scatter torch-sparse -f https://data.pyg.org/whl/torch-2.0.1+${CUDA}.html
-pip install torch-cluster -f https://data.pyg.org/whl/torch-2.0.0+${CUDA}.html
-
+# Note that not all combinations of torch and CUDA are available
+# See https://github.com/pyg-team/pyg-lib to check the configuration that works for you
+TORCH="2.3.0"   # available options: 1.12.0, 1.13.0, 2.0.0, 2.1.0, 2.2.0, or 2.3.0
+CUDA="cu121"    # if available, select the CUDA version suitable for your system
+                # available options: cpu, cu102, cu113, cu116, cu117, cu118, or cu121
+pip install torch==${TORCH} --extra-index-url https://download.pytorch.org/whl/${CUDA}
+pip install lightning torch_geometric==2.4.0
+pip install pyg-lib torch-scatter torch-sparse torch-cluster -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
 pytest
 
 pre-commit install

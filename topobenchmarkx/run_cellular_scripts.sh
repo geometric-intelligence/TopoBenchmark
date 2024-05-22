29678620e85f5b97e6514bb7b18fb3e0b25e5367
gbg141 <guille_gbg@hotmail.com>
2024-05-22 19:06:00 +0200

    Cleaning

--- topobenchmarkx/run_cellular_scripts.sh
+++
@@ -1,9 +0,0 @@
-# Run the scripts from the hypergraph directory
-bash /TopoBenchmarkX/hp_scripts/main_exp/cellular/CWN.sh
-bash /TopoBenchmarkX/hp_scripts/main_exp/cellular/CCCN.sh
-bash /TopoBenchmarkX/hp_scripts/main_exp/cellular/CAN.sh
-bash /TopoBenchmarkX/hp_scripts/main_exp/cellular/CCXN.sh
-
-# Run in case we have time
-# bash ~/TopoBenchmarkX/hp_scripts/main_exp/cellular/left_out.sh
-

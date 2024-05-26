24857c6a064d2d90b7143d72af3a5e1b43410e30
levtelyatnikov <tlscabinet@gmail.com>
2024-05-26 04:24:13 +0200

    update simplicial scripts

--- hp_scripts/main_exp/simplicial/tu/run_tu.sh
+++ hp_scripts/main_exp/simplicial/tu/run_tu.sh
@@ -2,10 +2,9 @@
 bash ../../../hp_scripts/main_exp/simplicial/tu/sccn_gpu_0.sh &
 bash ../../../hp_scripts/main_exp/simplicial/tu/sccn_gpu_1.sh &
 
-# SCCNN (FINISH NCIs)
-bash ../../../hp_scripts/main_exp/simplicial/tu/sccnn_gpu_2.sh &
-bash ../../../hp_scripts/main_exp/simplicial/tu/sccnn_gpu_3.sh 
-
+# # SCCNN (FINISH NCIs) RUNNING THESE ONES ON DIAG MACHINE
+# bash ../../../hp_scripts/main_exp/simplicial/tu/sccnn_gpu_2.sh &
+# bash ../../../hp_scripts/main_exp/simplicial/tu/sccnn_gpu_3.sh 
 
 # SCN (Make NCIs)
 bash ../../../hp_scripts/main_exp/simplicial/tu/scn_gpu_2.sh &

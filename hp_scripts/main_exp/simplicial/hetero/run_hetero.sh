974e9c4cc19fd6580cc873acfc7f3331e40b9817
Guillermo <guille_gbg@hotmail.com>
2024-05-28 04:54:09 -0700

    Scripts updated

--- hp_scripts/main_exp/simplicial/hetero/run_hetero.sh
+++ hp_scripts/main_exp/simplicial/hetero/run_hetero.sh
@@ -1,5 +1,6 @@
-bash ../../../hp_scripts/main_exp/simplicial/hetero/scn_gpu_0.sh &
-bash ../../../hp_scripts/main_exp/simplicial/hetero/scn_gpu_2.sh &
-bash ../../../hp_scripts/main_exp/simplicial/hetero/scn_gpu_3.sh &
-bash ../../../hp_scripts/main_exp/simplicial/hetero/scn_gpu_1.sh 
+# bash ../../../hp_scripts/main_exp/simplicial/hetero/scn_gpu_0.sh &
+# bash ../../../hp_scripts/main_exp/simplicial/hetero/scn_gpu_2.sh &
+# bash ../../../hp_scripts/main_exp/simplicial/hetero/scn_gpu_3.sh &
+# bash ../../../hp_scripts/main_exp/simplicial/hetero/scn_gpu_1.sh &
+bash ../../../hp_scripts/main_exp/simplicial/hetero/scn_cpu.sh &
 

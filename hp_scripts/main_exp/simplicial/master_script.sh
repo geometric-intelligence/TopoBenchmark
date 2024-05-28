d2e6e389ef00583d40dd2c4da9fbd18fdc1c7636
Guillermo <guille_gbg@hotmail.com>
2024-05-28 15:51:26 -0700

    keep master

--- hp_scripts/main_exp/simplicial/master_script.sh
+++ hp_scripts/main_exp/simplicial/master_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-bash ./hetero/run_hetero.sh
+# bash ./hetero/run_hetero.sh
+# wait
+
+bash ./tu/run_tu.sh
 wait
 
-# bash ./tu/run_tu.sh
-# wait
-
-# bash ./zinc/run_zinc.sh
-# wait
+bash ./zinc/run_zinc.sh
+wait

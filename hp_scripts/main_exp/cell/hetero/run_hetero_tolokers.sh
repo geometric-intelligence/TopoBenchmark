b3fb500c53745c8ccd9b9db9f2a8d4a9568bb513
levtelyatnikov <tlscabinet@gmail.com>
2024-05-29 21:47:36 +0200

    run larger datasets

---
+++ hp_scripts/main_exp/cell/hetero/run_hetero_tolokers.sh
@@ -0,0 +1,5 @@
+bash ../../../hp_scripts/main_exp/cell/hetero/tolokers_ccxn_gpu_0.sh &
+bash ../../../hp_scripts/main_exp/cell/hetero/tolokers_cwn_gpu_1.sh &
+bash ../../../hp_scripts/main_exp/cell/hetero/tolokers_cccn_gpu_2.sh
+ 
+

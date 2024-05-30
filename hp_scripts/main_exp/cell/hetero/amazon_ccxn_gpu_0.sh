6ad5efce88a54409510313f8fdc12f5ce4a847e0
Guille <guille_gbg@hotmail.com>
2024-05-30 09:05:38 +0000

    solved bugs in scripts

--- hp_scripts/main_exp/cell/hetero/amazon_ccxn_gpu_0.sh
+++ hp_scripts/main_exp/cell/hetero/amazon_ccxn_gpu_0.sh
@@ -1,19 +1,19 @@
 datasets=( amazon_ratings )
 
-seeds=(0 3 5 7 9)
+seeds=(32 64 128)
 
 for seed in ${seeds[*]}
 do 
   for dataset in ${datasets[*]}
   do
-  python train.py \
+  python ../../../topobenchmarkx/train.py \
     dataset=$dataset \
     model=cell/ccxn \
     model.optimizer.lr=0.001 \
-    model.feature_encoder.out_channels=32,64,128 \
+    model.feature_encoder.out_channels=$seed \
     model.backbone.n_layers=1,2,3,4 \
     model.feature_encoder.proj_dropout=0.25,0.5 \
-    dataset.parameters.data_seed=$seed \
+    dataset.parameters.data_seed=0,3,5,7,9 \
     dataset.parameters.batch_size=1 \
     dataset.transforms.graph2cell_lifting.max_cell_length=10 \
     model.readout.readout_name="NoReadOut,PropagateSignalDown" \
@@ -25,4 +25,5 @@ do
     callbacks.early_stopping.patience=50 \
     tags="[MainExperiment]" \
     --multirun &
+done
 done

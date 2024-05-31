25a1b5eeddaa873400bc14f8e4b979152df19da7
guille <guille_gbg@hotmail.com>
2024-05-31 23:15:12 +0000

    tolokers

--- hp_scripts/main_exp/simplicial/tu/run_tu2.sh
+++ hp_scripts/main_exp/simplicial/tu/run_tu2.sh
@@ -1,3 +1,3 @@
-bash ../../../hp_scripts/main_exp/simplicial/tu/reddit_sccn_gpu_0.sh &
-bash ../../../hp_scripts/main_exp/simplicial/tu/reddit_sccn_gpu_1.sh &
-bash ../../../hp_scripts/main_exp/simplicial/tu/reddit_scn_gpu_2.sh 
+#bash ../../../hp_scripts/main_exp/simplicial/tu/reddit_sccn_gpu_0.sh &
+bash ../../../hp_scripts/main_exp/simplicial/tu/reddit_sccnn_gpu_1.sh &
+#bash ../../../hp_scripts/main_exp/simplicial/tu/reddit_scn_gpu_2.sh 

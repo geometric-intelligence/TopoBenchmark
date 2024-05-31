b97ed6f7174fcc3b815b3f5bd755d779e231229d
Coerulatus <marcomontagna21@gmail.com>
2024-05-31 15:05:07 +0000

    added skillings_mack test to handle nans

--- topobenchmarkx/evaluators/comparisons.py
+++ topobenchmarkx/evaluators/comparisons.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.stats import friedmanchisquare, wilcoxon
+from scipy.stats import friedmanchisquare, wilcoxon, rankdata, chi2
 
 
 def signed_ranks_test(results_1, results_2):
@@ -33,10 +33,89 @@ def friedman_test(results):
     res = [r for r in results]
     return friedmanchisquare(*res)[1]
 
+def skillings_mack_test(results, n_simulations=10000):
+    r"""Calculates the p-value of the Skillings-Mack test between M models (treatments) on N
+    datasets (blocks). This test is a general Friedman-type statistic that can be used in
+    almost any block design with an arbitrary missing-data structure. 
+    
+    https://www.stat.cmu.edu/technometrics/80-89/VOL-23-02/v2302171.pdf
+    
+    Args:
+        results (numpy.array): A MxN numpy array with the results of M models.
+        n_simulations (int, optional): The number of simulations to perform to estimate the p-value. (default: 10000)
+    Returns:
+        float: The p-value of the test.
+    """
+    def get_SM_value(results):
+        r"""Calculates the Skillings-Mack statistic for a given set of results.
+        
+        Args:
+            results (numpy.array): A MxN numpy array with the results of M models.
+        Returns:
+            float: The Skillings-Mack statistic.
+        """
+        M, N = results.shape
+        cols_to_remove = []
+        for i in range(N):
+            if np.sum(~np.isnan(results[:, i])) <= 1:
+                cols_to_remove.append(i)
+        results = np.delete(results, cols_to_remove, axis=1)
+        k = np.sum(~np.isnan(results), axis=0)
+
+        ranks = rankdata(-results, axis=0, method='average', nan_policy='omit')
+        
+        A = np.zeros(M)
+        for j in range(M):
+            for i in range(N):
+                if ~np.isnan(ranks[j, i]):
+                    A[j] += np.power(12/(k[i]+1), 0.5) * (ranks[j, i] - (k[i]+1)/2)
+        
+        covariance_matrix = np.zeros((M, M))
+        for i in range(M):
+            for j in range(M):
+                if i != j:
+                    covariance_matrix[i, j] = -np.sum(~np.isnan(results[i]) & ~np.isnan(results[j]))
+        for i in range(M):
+            covariance_matrix[i, i] = -np.sum(covariance_matrix[i, :])
+        rank = np.linalg.matrix_rank(covariance_matrix)
+        if rank == M-1:
+            covariance_matrix = np.pad(covariance_matrix[:-1,:-1], ((0, 1), (0, 1)), mode='constant')
+        else:
+            raise ValueError(f"Problem with the covariance matrix having rank {rank} instead of {M-1} (M-1).")
+        inv_covariance_matrix = np.linalg.pinv(covariance_matrix)
+        SM = np.dot(A.T, np.dot(inv_covariance_matrix, A))
+        return SM
+    
+    def get_simulated_SM_values(res, n_nans, n_simulations):
+        r"""Calculates the Skillings-Mack statistic for the null hypothesis. The null hypothesis
+        is that the models are equivalent and the differences between them are due to random noise.
+        For this reason we use the normal distribution to obtain the simulated results.
+        
+        Args:
+            res (numpy.array): A MxN numpy array with the results of M models.
+            n_nans (numpy.array): A numpy array with the number of NaNs per row.
+            n_simulations (int): The number of simulations to perform.
+        """
+        res_shape = res.shape
+        SMs = np.zeros(n_simulations)
+        for i in range(n_simulations):
+            res = np.random.normal(loc=0, scale=1, size=res_shape)
+            for j in range(res_shape[0]):
+                res[j, np.random.choice(res_shape[0], n_nans[j])] = np.nan
+            SMs[i] = get_SM_value(res)
+        return SMs
+    
+    SM = get_SM_value(results)
+    p1 = chi2.sf(SM, results.shape[0]-1)
+    n_nans = np.sum(np.isnan(results), axis=1)
+    null_SM = get_simulated_SM_values(results, n_nans, n_simulations)
+    p = np.sum(null_SM > SM) / n_simulations
+
+    return p
 
 def compare_models(results, p_limit=0.05, verbose=False):
-    """Compares different models. First it uses the Friedman test to check that
-    the models are significantly different, then it uses pairwise comparisons
+    """Compares different models. First it uses the Friedman test or the Skillings-Mack test 
+    to check that the models are significantly different, then it uses pairwise comparisons
     to study the ranking of the models.
 
     Args:
@@ -50,13 +129,23 @@ def compare_models(results, p_limit=0.05, verbose=False):
     """
     M = results.shape[0]
 
-    average_ranks = np.mean(np.argsort(-results2, axis=0) + 1, axis=1)
-
-    friedman_result = friedman_test(results)
-    if friedman_result > p_limit:
+    ranks = rankdata(-results, axis=0, nan_policy='omit')
+    
+    k = np.sum(~np.isnan(results), axis=0)
+    for i in range(M):
+        ranks[i, np.isnan(results[i])] = (k[i] +1) / 2
+    average_ranks = np.mean(ranks, axis=1)
+    
+    if np.any(np.isnan(results)):
+        p_value = skillings_mack_test(results)
+        test_name = "Skillings-Mack"
+    else:
+        p_value = friedman_test(results)
+        test_name = "Friedman"
+    if p_value > p_limit:
         if verbose:
             print(
-                f"The Friedman test confirmed the null-hypothesis with p-value of {friedman_result}"
+                f"The {test_name} test confirmed the null-hypothesis with p-value of {p_value}"
             )
         return average_ranks, [[i for i in range(M)]]
 
@@ -118,3 +207,13 @@ if __name__ == "__main__":
     print("-"*50)
     print("Compare models with similar results:")
     print(compare_models(results3, verbose=True))
+    
+    results4 = np.array(
+        [
+            [    3.2,    3.1,    4.3,    3.5,    3.6,    4.5, np.nan,    4.3,    3.5],
+            [    4.1,    3.9,    3.5,    3.6,    4.2,    4.7,    4.2,    4.6, np.nan],
+            [    3.8,    3.4,    4.6,    3.9,    3.7,    3.7,    3.4,    4.4,    3.7],
+            [    4.2,    4.0,    4.8,    4.0,    3.9, np.nan, np.nan,    4.9,    3.9],
+        ]
+    )
+    skillings_mack_test(-results4)

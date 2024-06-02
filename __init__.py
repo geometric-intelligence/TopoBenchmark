e390681d38b18eec203cddc956bed7ebd9b5e5ed
gbg141 <guille_gbg@hotmail.com>
2024-06-03 01:19:27 +0200

    Last try

--- __init__.py
+++ __init__.py
@@ -1,17 +1,13 @@
+# numpydoc ignore=GL08
 import test
 
 import configs
 import topobenchmarkx
-import topobenchmarkx.nn.backbones
-
-from .topobenchmarkx import run
 
 __all__ = [
     "topobenchmarkx",
-    "topobenchmarkx.nn.backbones",
     "configs",
     "test",
-    "run",
 ]
 
 __version__ = "0.0.1"

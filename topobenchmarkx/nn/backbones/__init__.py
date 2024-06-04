6190360cfa13f4c6a83a276bd25acd031b03b4ab
levtelyatnikov <tlscabinet@gmail.com>
2024-06-04 21:00:17 +0200

    updated backbones/init.py

--- topobenchmarkx/nn/backbones/__init__.py
+++ topobenchmarkx/nn/backbones/__init__.py
@@ -2,14 +2,12 @@
 
 from .cell import (
     CCCN,
-    CWN,
 )
 from .hypergraph import EDGNN
 from .simplicial import SCCNNCustom
 
 __all__ = [
     "CCCN",
-    "CWN",
     "EDGNN",
     "SCCNNCustom",
 ]

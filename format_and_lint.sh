a091a4485f69c8f379feb41140e5926027f046eb
gbg141 <guille_gbg@hotmail.com>
2024-06-07 13:53:45 +0200

    Formatters

--- format_and_lint.sh
+++ format_and_lint.sh
@@ -2,9 +2,3 @@
 
 # Run ruff to check for issues and fix them
 ruff check . --fix
-
-# Run docformatter to reformat docstrings and comments
-docformatter --in-place --recursive --wrap-summaries 79 --wrap-descriptions 79 .
-
-# Run black to format the code
-black .

58a97d2b06426dbd5b6f60f60b11d2130d54fc71
levtelyatnikov <tlscabinet@gmail.com>
2024-06-12 02:18:53 +0200

    test instantiators of logger and callbacks

---
+++ test/utils/test_instantiators.py
@@ -0,0 +1,27 @@
+"""Unit tests for config instantiators."""
+
+import pytest
+from omegaconf import OmegaConf, DictConfig
+from topobenchmarkx.utils.instantiators import (
+    instantiate_callbacks,
+    instantiate_loggers
+)
+
+class TestConfigInstantiators:
+    """Test config instantiators."""
+
+    def setup_method(self):
+        """Setup method."""
+        self.callback = OmegaConf.load("configs/callbacks/model_summary.yaml")
+        self.logger = DictConfig({'_target_': 'lightning.pytorch.loggers.wandb.WandbLogger', 'save_dir': '/', 'offline': False, 'id': None, 'anonymous': None, 'project': 'None', 'log_model': False, 'prefix': '', 'group': '', 'tags': [], 'job_type': ''})
+
+    def test_instantiate_callbacks(self):
+        """Test instantiate_callbacks."""
+        result = instantiate_callbacks(self.callback)
+        assert type(result) == list
+       
+    def test_instantiate_loggers(self):
+        """Test instantiate_loggers."""
+        result = instantiate_loggers(self.logger)
+        assert type(result) == list
+        

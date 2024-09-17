5051ef4f36b5ba6aa3d788671c257475db79f834
Guillermo <guille_gbg@hotmail.com>
2024-09-16 18:09:01 -0700

    Fix env_setup.sh

--- env_setup.sh
+++ env_setup.sh
@@ -5,7 +5,7 @@ pip install -e '.[all]'
 # Note that not all combinations of torch and CUDA are available
 # See https://github.com/pyg-team/pyg-lib to check the configuration that works for you
 TORCH="2.3.0"   # available options: 1.12.0, 1.13.0, 2.0.0, 2.1.0, 2.2.0, or 2.3.0
-CUDA="cu121"    # if available, select the CUDA version suitable for your system
+CUDA="cu118"    # if available, select the CUDA version suitable for your system
                 # available options: cpu, cu102, cu113, cu116, cu117, cu118, or cu121
 pip install torch==${TORCH} --extra-index-url https://download.pytorch.org/whl/${CUDA}
 pip install pyg-lib torch-scatter torch-sparse torch-cluster -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html

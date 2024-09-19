daf554ed1ec35f4d81985de91ec351cb2e1d9c98
Guillermo Bernárdez <guille_gbg@hotmail.com>
2024-09-18 18:04:03 -0700

    ZINC setup

---
+++ ZINC.sh
@@ -0,0 +1,172 @@
+python -m topobenchmarkx \
+    dataset=graph/ZINC \
+    model=simplicial/tune \
+    model.feature_encoder.out_channels=32 \
+    model.tune_gnn=GCN,GIN,GraphSAGE \
+    model.backbone.GNN.num_layers=1,2 \
+    model.backbone.routes=\[\[\[0,0\],up\],\[\[1,1\],down\],\[\[1,1\],up\],\[\[2,2\],down\]\],\[\[\[0,0\],up\],\[\[0,1\],cob\],\[\[1,0\],boundary\],\[\[1,1\],down\],\[\[1,1\],up\],\[\[1,2\],cob\],\[\[2,1\],boundary\],\[\[2,2\],down\]\],\[\[\[0,0\],up\],\[\[1,0\],boundary\],\[\[1,1\],up\],\[\[2,1\],boundary\]\],\[\[\[0,0\],up\],\[\[0,1\],cob\],\[\[1,1\],up\],\[\[1,2\],cob\]\],\[\[\[0,0\],up\],\[\[1,0\],boundary\],\[\[1,1\],down\],\[\[1,1\],up\],\[\[2,1\],boundary\],\[\[2,2\],down\]\],\[\[\[0,0\],up\],\[\[0,1\],cob\],\[\[1,1\],down\],\[\[1,1\],up\],\[\[1,2\],cob\],\[\[2,2\],down\]\] \
+    model.backbone.layers=2,4 \
+    model.feature_encoder.proj_dropout=0.3 \
+    seed=1,3,5,7,9 \
+    transforms.one_hot_node_degree_features.degrees_fields=x \
+    model.readout.readout_name=PropagateSignalDown \
+    logger.wandb.project=TopoTune_ZINC \
+    trainer.max_epochs=1000 \
+    trainer.min_epochs=50 \
+    trainer.devices=\[0\] \
+    trainer.check_val_every_n_epoch=1 \
+    callbacks.early_stopping.patience=50 \
+    tags="[FirstExperiments]" \
+    --multirun &
+
+python -m topobenchmarkx \
+    dataset=graph/ZINC \
+    model=simplicial/tune \
+    model.feature_encoder.out_channels=32 \
+    model.tune_gnn=GAT \
+    +model.backbone.GNN.heads=1,2,4 \
+    model.backbone.GNN.num_layers=1,2 \
+    model.backbone.routes=\[\[\[0,0\],up\],\[\[1,1\],down\],\[\[1,1\],up\],\[\[2,2\],down\]\],\[\[\[0,0\],up\],\[\[0,1\],cob\],\[\[1,0\],boundary\],\[\[1,1\],down\],\[\[1,1\],up\],\[\[1,2\],cob\],\[\[2,1\],boundary\],\[\[2,2\],down\]\],\[\[\[0,0\],up\],\[\[1,0\],boundary\],\[\[1,1\],up\],\[\[2,1\],boundary\]\],\[\[\[0,0\],up\],\[\[0,1\],cob\],\[\[1,1\],up\],\[\[1,2\],cob\]\],\[\[\[0,0\],up\],\[\[1,0\],boundary\],\[\[1,1\],down\],\[\[1,1\],up\],\[\[2,1\],boundary\],\[\[2,2\],down\]\],\[\[\[0,0\],up\],\[\[0,1\],cob\],\[\[1,1\],down\],\[\[1,1\],up\],\[\[1,2\],cob\],\[\[2,2\],down\]\] \
+    model.backbone.layers=2,4 \
+    model.feature_encoder.proj_dropout=0.3 \
+    seed=1,3,5,7,9 \
+    transforms.one_hot_node_degree_features.degrees_fields=x \
+    model.readout.readout_name=PropagateSignalDown \
+    logger.wandb.project=TopoTune_ZINC \
+    trainer.max_epochs=1000 \
+    trainer.min_epochs=50 \
+    trainer.devices=\[1\] \
+    trainer.check_val_every_n_epoch=1 \
+    callbacks.early_stopping.patience=50 \
+    tags="[FirstExperiments]" \
+    --multirun &
+
+python -m topobenchmarkx \
+    dataset=graph/ZINC \
+    model=simplicial/tune \
+    model.feature_encoder.out_channels=32 \
+    model.tune_gnn=GCN,GIN,GraphSAGE \
+    model.backbone.GNN.num_layers=1,2 \
+    model.backbone.routes=\[\[\[0,0\],up\],\[\[1,1\],down\]\],\[\[\[0,0\],up\],\[\[1,1\],up\]\],\[\[\[0,0\],up\],\[\[1,1\],up\],\[\[1,1\],down\]\],\[\[\[0,0\],up\],\[\[2,1\],incidence\]\] \
+    model.backbone.layers=2,4 \
+    model.feature_encoder.proj_dropout=0.3 \
+    seed=1,3,5,7,9 \
+    transforms.one_hot_node_degree_features.degrees_fields=x \
+    model.readout.readout_name=PropagateSignalDown \
+    logger.wandb.project=TopoTune_ZINC \
+    trainer.max_epochs=1000 \
+    trainer.min_epochs=50 \
+    trainer.devices=\[2\] \
+    trainer.check_val_every_n_epoch=1 \
+    callbacks.early_stopping.patience=50 \
+    tags="[FirstExperiments]" \
+    --multirun &
+
+python -m topobenchmarkx \
+    dataset=graph/ZINC \
+    model=simplicial/tune \
+    model.feature_encoder.out_channels=32 \
+    model.tune_gnn=GAT \
+    +model.backbone.GNN.heads=1,2,4 \
+    model.backbone.GNN.num_layers=1,2 \
+    model.backbone.routes=\[\[\[0,0\],up\],\[\[1,1\],down\]\],\[\[\[0,0\],up\],\[\[1,1\],up\]\],\[\[\[0,0\],up\],\[\[1,1\],up\],\[\[1,1\],down\]\],\[\[\[0,0\],up\],\[\[2,1\],incidence\]\] \
+    model.backbone.layers=2,4 \
+    model.feature_encoder.proj_dropout=0.3 \
+    seed=1,3,5,7,9 \
+    transforms.one_hot_node_degree_features.degrees_fields=x \
+    model.readout.readout_name=PropagateSignalDown \
+    logger.wandb.project=TopoTune_ZINC \
+    trainer.max_epochs=1000 \
+    trainer.min_epochs=50 \
+    trainer.devices=\[3\] \
+    trainer.check_val_every_n_epoch=1 \
+    callbacks.early_stopping.patience=50 \
+    tags="[FirstExperiments]" \
+    --multirun &
+
+python -m topobenchmarkx \
+    dataset=graph/ZINC \
+    model=cell/tune \
+    model.feature_encoder.out_channels=32 \
+    model.tune_gnn=GCN,GIN,GraphSAGE \
+    model.backbone.GNN.num_layers=1,2 \
+    model.backbone.routes=\[\[\[0,0\],up\],\[\[1,1\],down\],\[\[1,1\],up\],\[\[2,2\],down\]\],\[\[\[0,0\],up\],\[\[0,1\],cob\],\[\[1,0\],boundary\],\[\[1,1\],down\],\[\[1,1\],up\],\[\[1,2\],cob\],\[\[2,1\],boundary\],\[\[2,2\],down\]\],\[\[\[0,0\],up\],\[\[1,0\],boundary\],\[\[1,1\],up\],\[\[2,1\],boundary\]\],\[\[\[0,0\],up\],\[\[0,1\],cob\],\[\[1,1\],up\],\[\[1,2\],cob\]\],\[\[\[0,0\],up\],\[\[1,0\],boundary\],\[\[1,1\],down\],\[\[1,1\],up\],\[\[2,1\],boundary\],\[\[2,2\],down\]\],\[\[\[0,0\],up\],\[\[0,1\],cob\],\[\[1,1\],down\],\[\[1,1\],up\],\[\[1,2\],cob\],\[\[2,2\],down\]\] \
+    model.backbone.layers=2,4 \
+    model.feature_encoder.proj_dropout=0.3 \
+    seed=1,3,5,7,9 \
+    transforms.one_hot_node_degree_features.degrees_fields=x \
+    model.readout.readout_name=PropagateSignalDown \
+    logger.wandb.project=TopoTune_ZINC \
+    trainer.max_epochs=1000 \
+    trainer.min_epochs=50 \
+    trainer.devices=\[4\] \
+    trainer.check_val_every_n_epoch=1 \
+    callbacks.early_stopping.patience=50 \
+    tags="[FirstExperiments]" \
+    --multirun &
+
+python -m topobenchmarkx \
+    dataset=graph/ZINC \
+    model=cell/tune \
+    model.feature_encoder.out_channels=32 \
+    model.tune_gnn=GAT \
+    +model.backbone.GNN.heads=1,2,4 \
+    model.backbone.GNN.num_layers=1,2 \
+    model.backbone.routes=\[\[\[0,0\],up\],\[\[1,1\],down\],\[\[1,1\],up\],\[\[2,2\],down\]\],\[\[\[0,0\],up\],\[\[0,1\],cob\],\[\[1,0\],boundary\],\[\[1,1\],down\],\[\[1,1\],up\],\[\[1,2\],cob\],\[\[2,1\],boundary\],\[\[2,2\],down\]\],\[\[\[0,0\],up\],\[\[1,0\],boundary\],\[\[1,1\],up\],\[\[2,1\],boundary\]\],\[\[\[0,0\],up\],\[\[0,1\],cob\],\[\[1,1\],up\],\[\[1,2\],cob\]\],\[\[\[0,0\],up\],\[\[1,0\],boundary\],\[\[1,1\],down\],\[\[1,1\],up\],\[\[2,1\],boundary\],\[\[2,2\],down\]\],\[\[\[0,0\],up\],\[\[0,1\],cob\],\[\[1,1\],down\],\[\[1,1\],up\],\[\[1,2\],cob\],\[\[2,2\],down\]\] \
+    model.backbone.layers=2,4 \
+    model.feature_encoder.proj_dropout=0.3 \
+    seed=1,3,5,7,9 \
+    transforms.one_hot_node_degree_features.degrees_fields=x \
+    model.readout.readout_name=PropagateSignalDown \
+    logger.wandb.project=TopoTune_ZINC \
+    trainer.max_epochs=1000 \
+    trainer.min_epochs=50 \
+    trainer.devices=\[5\] \
+    trainer.check_val_every_n_epoch=1 \
+    callbacks.early_stopping.patience=50 \
+    tags="[FirstExperiments]" \
+    --multirun &
+
+python -m topobenchmarkx \
+    dataset=graph/ZINC \
+    model=cell/tune \
+    model.feature_encoder.out_channels=32 \
+    model.tune_gnn=GCN,GIN,GraphSAGE \
+    model.backbone.GNN.num_layers=1,2 \
+    model.backbone.routes=\[\[\[0,0\],up\],\[\[1,1\],down\]\],\[\[\[0,0\],up\],\[\[1,1\],up\]\],\[\[\[0,0\],up\],\[\[1,1\],up\],\[\[1,1\],down\]\],\[\[\[0,0\],up\],\[\[2,1\],incidence\]\] \
+    model.backbone.layers=2,4 \
+    model.feature_encoder.proj_dropout=0.3 \
+    seed=1,3,5,7,9 \
+    transforms.one_hot_node_degree_features.degrees_fields=x \
+    model.readout.readout_name=PropagateSignalDown \
+    logger.wandb.project=TopoTune_ZINC \
+    trainer.max_epochs=1000 \
+    trainer.min_epochs=50 \
+    trainer.devices=\[6\] \
+    trainer.check_val_every_n_epoch=1 \
+    callbacks.early_stopping.patience=50 \
+    tags="[FirstExperiments]" \
+    --multirun &
+
+python -m topobenchmarkx \
+    dataset=graph/ZINC \
+    model=cell/tune \
+    model.feature_encoder.out_channels=32 \
+    model.tune_gnn=GAT \
+    +model.backbone.GNN.heads=1,2,4 \
+    model.backbone.GNN.num_layers=1,2 \
+    model.backbone.routes=\[\[\[0,0\],up\],\[\[1,1\],down\]\],\[\[\[0,0\],up\],\[\[1,1\],up\]\],\[\[\[0,0\],up\],\[\[1,1\],up\],\[\[1,1\],down\]\],\[\[\[0,0\],up\],\[\[2,1\],incidence\]\] \
+    model.backbone.layers=2,4 \
+    model.feature_encoder.proj_dropout=0.3 \
+    seed=1,3,5,7,9 \
+    transforms.one_hot_node_degree_features.degrees_fields=x \
+    model.readout.readout_name=PropagateSignalDown \
+    logger.wandb.project=TopoTune_ZINC \
+    trainer.max_epochs=1000 \
+    trainer.min_epochs=50 \
+    trainer.devices=\[7\] \
+    trainer.check_val_every_n_epoch=1 \
+    callbacks.early_stopping.patience=50 \
+    tags="[FirstExperiments]" \
+    --multirun &
+

ae4cba429dccc2cd7547e3c3bfe512feb0774d9c
Martin Carrasco <martin.carrasco@utec.edu.pe>
2024-11-04 16:25:14 +0100

    FIX: Custom dataloader removal

--- topobenchmarkx/dataloader/__init__.py
+++ topobenchmarkx/dataloader/__init__.py
@@ -1,7 +1,6 @@
 """This module implements the dataloader for the topobenchmarkx package."""
 
-from .custom_dataloader import CTBXDataloader
 from .dataload_dataset import DataloadDataset
 from .dataloader import TBXDataloader
 
-__all__ = ["TBXDataloader", "DataloadDataset", "CTBXDataloader"]
+__all__ = ["TBXDataloader", "DataloadDataset"]

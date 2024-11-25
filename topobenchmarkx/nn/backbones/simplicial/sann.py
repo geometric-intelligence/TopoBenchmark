FIX: automatic t-hop neighbourhood calculation based on config

topobenchmarkx/nn/backbones/simplicial/sann.py
@@ -2,6 +2,7 @@
 
 import torch
 import torch.nn.functional
+from torch.nn import ParameterList
 from torch.nn.parameter import Parameter
 
 
@@ -30,7 +31,7 @@ class SANN(torch.nn.Module):
         hidden_channels,
         update_func=None,
         complex_dim=3,
-        hop_num=2,
+        hop_num=3,
         n_layers=2,
     ):
         super().__init__()
@@ -40,16 +41,17 @@ class SANN(torch.nn.Module):
         assert n_layers >= 1
 
         if isinstance(in_channels, int):  # If only one value is passed
-            in_channels = [in_channels] * self.complex_dim
+            in_channels = [in_channels] * self.hop_num
 
         self.layers = torch.nn.ModuleList()
 
         # Set of simplices layers
         self.layers_0 = torch.nn.ModuleList(
             SANNLayer(
-                [in_channels[i] for i in range(complex_dim)],
-                [hidden_channels] * complex_dim,
+                [in_channels[i] for i in range(hop_num)],
+                [hidden_channels] * hop_num,
                 update_func=update_func,
+                hop_num=hop_num,
             )
             for i in range(complex_dim)
         )
@@ -60,9 +62,10 @@ class SANN(torch.nn.Module):
             self.layers.append(
                 torch.nn.ModuleList(
                     SANNLayer(
-                        [hidden_channels] * complex_dim,
-                        [hidden_channels] * complex_dim,
-                        update_func="lrelu",
+                        [hidden_channels] * hop_num,
+                        [hidden_channels] * hop_num,
+                        update_func=update_func,
+                        hop_num=hop_num,
                     )
                     for i in range(complex_dim)
                 )
@@ -114,6 +117,8 @@ class SANNLayer(torch.nn.Module):
         Number of input channels.
     out_channels : int
         Number of output channels.
+    hop_num : int
+        Number of hop representations to consider.
     aggr_norm : bool
         Whether to perform aggregation normalization.
     update_func : str
@@ -131,21 +136,22 @@ class SANNLayer(torch.nn.Module):
         self,
         in_channels,
         out_channels,
+        hop_num,
         aggr_norm: bool = False,
         update_func=None,
         initialization: str = "xavier_normal",
     ) -> None:
         super().__init__()
 
-        in_channels_0, in_channels_1, in_channels_2 = in_channels
-        out_channels_0, out_channels_1, out_channels_2 = out_channels
+        assert hop_num == len(
+            in_channels
+        ), "Number of hops must be equal to the number of input channels."
+        assert hop_num == len(
+            out_channels
+        ), "Number of hops must be equal to the number of output channels."
 
-        self.in_channels_0 = in_channels_0
-        self.in_channels_1 = in_channels_1
-        self.in_channels_2 = in_channels_2
-        self.out_channels_0 = out_channels_0
-        self.out_channels_1 = out_channels_1
-        self.out_channels_2 = out_channels_2
+        self.in_channels = in_channels
+        self.out_channels = out_channels
 
         self.aggr_norm = aggr_norm
         self.update_func = update_func
@@ -153,29 +159,27 @@ class SANNLayer(torch.nn.Module):
 
         assert initialization in ["xavier_uniform", "xavier_normal"]
 
-        self.weight_0 = Parameter(
-            torch.Tensor(
-                self.in_channels_0,
-                self.out_channels_0,
-            )
+        self.weights = ParameterList(
+            [
+                Parameter(
+                    torch.Tensor(
+                        self.in_channels[i],
+                        self.out_channels[i],
+                    )
+                )
+                for i in range(hop_num)
+            ]
         )
-
-        self.weight_1 = Parameter(
-            torch.Tensor(
-                self.in_channels_1,
-                self.out_channels_1,
-            )
+        self.biases = ParameterList(
+            [
+                Parameter(
+                    torch.Tensor(
+                        self.out_channels[i],
+                    )
+                )
+                for i in range(hop_num)
+            ]
         )
-        self.weight_2 = Parameter(
-            torch.Tensor(
-                self.in_channels_2,
-                self.out_channels_2,
-            )
-        )
-
-        self.biases_0 = Parameter(torch.Tensor(self.out_channels_0))
-        self.biases_1 = Parameter(torch.Tensor(self.out_channels_1))
-        self.biases_2 = Parameter(torch.Tensor(self.out_channels_2))
 
         self.reset_parameters()
 
@@ -188,21 +192,13 @@ class SANNLayer(torch.nn.Module):
             Gain for the weight initialization.
         """
         if self.initialization == "xavier_uniform":
-            torch.nn.init.xavier_uniform_(self.weight_0, gain=gain)
-            torch.nn.init.xavier_uniform_(self.weight_1, gain=gain)
-            torch.nn.init.xavier_uniform_(self.weight_2, gain=gain)
-
-            torch.nn.init.zeros_(self.biases_0)
-            torch.nn.init.zeros_(self.biases_1)
-            torch.nn.init.zeros_(self.biases_2)
+            for i in range(len(self.weights)):
+                torch.nn.init.xavier_uniform_(self.weights[i], gain=gain)
+                torch.nn.init.zeros_(self.biases[i])
         elif self.initialization == "xavier_normal":
-            torch.nn.init.xavier_normal_(self.weight_0, gain=gain)
-            torch.nn.init.xavier_normal_(self.weight_1, gain=gain)
-            torch.nn.init.xavier_normal_(self.weight_2, gain=gain)
-
-            torch.nn.init.zeros_(self.biases_0)
-            torch.nn.init.zeros_(self.biases_1)
-            torch.nn.init.zeros_(self.biases_2)
+            for i in range(len(self.weights)):
+                torch.nn.init.xavier_normal_(self.weights[i], gain=gain)
+                torch.nn.init.zeros_(self.biases[i])
         else:
             raise RuntimeError(
                 "Initialization method not recognized. "
@@ -248,14 +244,13 @@ class SANNLayer(torch.nn.Module):
             Output tensors for each 2-cell.
         """
         # Extract all cells to all cells
-        x_0 = x_all[0]
-        x_1 = x_all[1]
-        x_2 = x_all[2]
+        t = len(x_all)
+        x_k_t = {i: x_all[i] for i in range(t)}
 
-        # k-simplex to k-simplex
-        x_k_to_0 = torch.mm(x_0, self.weight_0)
-        x_k_to_1 = torch.mm(x_1, self.weight_1)
-        x_k_to_2 = torch.mm(x_2, self.weight_2)
+        y_k_t = {
+            i: torch.mm(x_k_t[i], self.weights[i]) + self.biases[i]
+            for i in range(t)
+        }
 
         # TODO Check aggregation as list of ys
         # Need to check that this einsums are correct
@@ -263,10 +258,7 @@ class SANNLayer(torch.nn.Module):
         # y_1 = torch.einsum("nik,iok->no", x_1_all, self.weight_1)
         # y_2 = torch.einsum("nik,iok->no", x_2_all, self.weight_2)
 
-        y_0 = x_k_to_0 + self.biases_0
-        y_1 = x_k_to_1 + self.biases_1
-        y_2 = x_k_to_2 + self.biases_2
-
         if self.update_func is None:
-            return y_0, y_1, y_2
-        return self.update(y_0), self.update(y_1), self.update(y_2)
+            return tuple(y_k_t.values())
+
+        return tuple([self.update(y_t) for y_t in y_k_t.values()])

topobenchmarkx/nn/readouts/sann.py
@@ -14,8 +14,9 @@ class SANNReadout(AbstractZeroCellReadOut):
     ----------
     **kwargs : dict
         Additional keyword arguments. It should contain the following keys:
+        - complex_dim (int): Dimension of the simplicial complex.
+        - max_hop (int): Maximum hop neighbourhood to consider.
         - hidden_dim_1 (int):  Dimension of the embeddings.
-        - hidden_dim_2 (int): Dimension of the hidden layers.
         - out_channels (int): Number of classes.
         - pooling_type (str): Type of pooling operationg
     """
@@ -23,12 +24,16 @@ class SANNReadout(AbstractZeroCellReadOut):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+        complex_dim = kwargs["complex_dim"]
+        max_hop = kwargs["max_hop"]
         hidden_dim = kwargs["hidden_dim"]
         out_channels = kwargs["out_channels"]
         pooling_type = kwargs["pooling_type"]
 
         self.linear = torch.nn.Sequential(
-            torch.nn.Linear(3 * 3 * hidden_dim, hidden_dim),
+            torch.nn.Linear(
+                complex_dim * (max_hop + 1) * hidden_dim, hidden_dim
+            ),
             torch.nn.LeakyReLU(),
             torch.nn.Linear(hidden_dim, hidden_dim),
             torch.nn.LeakyReLU(),

topobenchmarkx/transforms/data_manipulations/precompute_khop_features.py
@@ -27,7 +27,7 @@ class PrecomputeKHopFeatures(torch_geometric.transforms.BaseTransform):
     ) -> None:
         super().__init__()
         self.type = "precompute_khop_features"
-        self.complex_dim = complex_dim - 1
+        self.complex_dim = complex_dim
         self.max_hop = max_hop
 
     def __repr__(self) -> str:
@@ -47,12 +47,12 @@ class PrecomputeKHopFeatures(torch_geometric.transforms.BaseTransform):
             The transformed data.
         """
         T = self.max_hop
-        K = self.complex_dim  # TODO Check where to get this degree from
-        B = [torch.abs(data[f"incidence_{i+1}"]) for i in range(K + 1)]
-        UP = [torch.mm(B[i], B[i].T) for i in range(K + 1)]
-        DOWN = [torch.mm(B[i].T, B[i]) for i in range(K + 1)]
-        Bs = [B[i].to_dense() for i in range(K + 1)]
-        Bc = [B[i].T.to_dense() for i in range(K + 1)]
+        K = self.complex_dim
+        B = [torch.abs(data[f"incidence_{i+1}"]) for i in range(K)]
+        UP = [torch.mm(B[i], B[i].T) for i in range(K)]
+        DOWN = [torch.mm(B[i].T, B[i]) for i in range(K)]
+        Bs = [B[i].to_dense() for i in range(K)]
+        Bc = [B[i].T.to_dense() for i in range(K)]
 
         Bs_new = Bc  # [torch.ones_like(Bs[0])] * len(Bs)
         Bc_new = Bs  # [torch.ones_like(Bc[0])] * len(Bc)
@@ -72,15 +72,13 @@ class PrecomputeKHopFeatures(torch_geometric.transforms.BaseTransform):
         # Create a dictionary that stores the i-simplices, the
         # j-th hop features matrix
         x_all = {
-            f"x{i}_{j}": torch.ones(1)
-            for i in range(K + 1)
-            for j in range(T + 1)
+            f"x{i}_{j}": torch.ones(1) for i in range(K) for j in range(T + 1)
         }
 
         # TODO Sometimes te normalizations results in 0
         # for non-connected simplices which blows up
         # Compute normalization matrices D_k
-        for i in range(K + 1):
+        for i in range(K):
             LS = torch.mm(UP[i], x_is[i]).flatten()
             D_i = LS  # - torch.diag(torch.abs(UP[i].to_dense())).flatten()
             # Check no zeroes that blow up the division
@@ -106,7 +104,7 @@ class PrecomputeKHopFeatures(torch_geometric.transforms.BaseTransform):
                     torch.mm(D_i_i_minus_1, Bc[i - 1]), D_i_minus_1_i
                 )
 
-            if i < K:
+            if i < (K - 1):
                 D_i_i_plus_1 = torch.mm(Bs[i], x_is[i + 1]).flatten()
                 D_i_i_plus_1 = torch.diagflat(
                     torch.div(1, torch.sqrt(D_i_i_plus_1 + 1))
@@ -125,13 +123,13 @@ class PrecomputeKHopFeatures(torch_geometric.transforms.BaseTransform):
             UP[i] = torch.mm(torch.mm(D_i, UP[i]), D_i)
 
         # Set the information for the 0-hop embeddings
-        for i in range(K + 1):
+        for i in range(K):
             x_all[f"x{i}_0"] = data[f"x_{i}"]
 
         # For each hop t=1,...,T
         for t in range(1, T + 1):
             # For each k-simplex, k=0,...,K
-            for k in range(K + 1):
+            for k in range(K):
                 # Set everything to `None` as
                 # some representations are not available
                 Y_U_1 = None
@@ -143,7 +141,7 @@ class PrecomputeKHopFeatures(torch_geometric.transforms.BaseTransform):
                 # The highest order simplex does not have a higher order adjacency
                 Y_U_1 = torch.mm(UP[k], x_all[f"x{k}_{t-1}"])
                 adjacencies_embedding.append(Y_U_1)
-                if k < K:
+                if k < (K - 1):
                     Y_C_1 = torch.mm(Bc_new[k], x_all[f"x{k+1}_{t-1}"])
 
                     adjacencies_embedding.append(Y_C_1)
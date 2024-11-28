2872d0d3b2ffdeb9acacca07ed3ed73bbe7de36e
Martin Carrasco <martin.carrasco@utec.edu.pe>
2024-11-28 19:14:49 +0100

    FIX: Sann config

--- topobenchmarkx/nn/wrappers/simplicial/sann_wrapper.py
+++ topobenchmarkx/nn/wrappers/simplicial/sann_wrapper.py
@@ -40,7 +40,9 @@ class SANNWrapper(AbstractWrapper):
             Dictionary containing the model output.
         """
         hop_data_names = [
-            k for k in batch if k.startswith("x") and len(k.split("_")[0]) == 2
+            k
+            for k, v in batch
+            if k.startswith("x") and len(k.split("_")[0]) == 2
         ]
         max_simplex_dim = max(
             [int(k.split("_")[0][1]) for k in hop_data_names]

FIX: SANN dimensions and add layer/batch normalization

topobenchmarkx/nn/backbones/simplicial/sann.py
@@ -23,6 +23,8 @@ class SANN(torch.nn.Module):
         Number of hops.
     n_layers : int
         Number of layers.
+    layer_norm : bool, optional
+        Wether to perform layer normalization.
     """
 
     def __init__(
@@ -33,12 +35,17 @@ class SANN(torch.nn.Module):
         complex_dim=3,
         max_hop=3,
         n_layers=2,
+        layer_norm=None,
     ):
         super().__init__()
         self.complex_dim = complex_dim
         self.max_hop = max_hop
+        self.layer_norm = layer_norm
 
         assert n_layers >= 1
+
+        if self.layer_norm:
+            self.layernorm = torch.nn.LayerNorm(hidden_channels, eps=1e-6)
 
         if isinstance(in_channels, int):  # If only one value is passed
             in_channels = [in_channels] * self.max_hop
@@ -101,7 +108,12 @@ class SANN(torch.nn.Module):
             # For each i-simplex (i=0,1,2) to all other k-simplices
             for i in range(self.complex_dim):
                 # Goes from i-simplex to all other simplices k<=i
-                x_i_to_t = layer[i](x[i])
+                x_i_to_t = (
+                    [self.layernorm(x_j) for x_j in x[i]]
+                    if self.layer_norm
+                    else x[i]
+                )
+                x_i_to_t = layer[i](x_i_to_t)
                 # Update the i-th simplex to all other simplices embeddings
                 x_i.append(tuple(x_i_to_t))
             x = tuple(x_i)

topobenchmarkx/nn/encoders/sann_encoder.py
@@ -25,6 +25,8 @@ class SANNFeatureEncoder(AbstractFeatureEncoder):
         List of indexes to apply the BaseEncoders to (default: None).
     max_hop : list[int], optional
         List of indexes to apply the BaseEncoders to in terms of hops (default: None).
+    batch_norm : bool, optional
+        Wether to apply batch normalizaiton when encoding (default: False).
     **kwargs : dict, optional
         Additional keyword arguments.
     """
@@ -36,6 +38,7 @@ class SANNFeatureEncoder(AbstractFeatureEncoder):
         proj_dropout=0,
         selected_dimensions=None,
         max_hop=3,
+        batch_norm=False,
         **kwargs,
     ):
         super().__init__()
@@ -58,6 +61,7 @@ class SANNFeatureEncoder(AbstractFeatureEncoder):
                         self.in_channels[i][j],
                         self.out_channels,
                         dropout=proj_dropout,
+                        batch_norm=batch_norm,
                     ),
                 )
 
@@ -105,15 +109,24 @@ class SimpleEncoder(torch.nn.Module):
         Dimensions of output features.
     dropout : float, optional
         Percentage of channels to discard between the two linear layers (default: 0).
+    batch_norm : bool, optional
+        Wether to perform batch normalization (default: False).
     """
 
-    def __init__(self, in_channels, out_channels, dropout=0):
+    def __init__(self, in_channels, out_channels, dropout=0, batch_norm=False):
         super().__init__()
+        self.batch_norm = batch_norm
         self.linear1 = torch.nn.Linear(in_channels, out_channels)
         # self.linear2 = torch.nn.Linear(out_channels, out_channels)
         self.relu = torch.nn.ReLU()
-        self.BN = GraphNorm(out_channels)
-        # self.dropout = torch.nn.Dropout(dropout)
+        self.BN = (
+            torch.nn.BatchNorm1d(out_channels)
+            if batch_norm
+            else torch.nn.Identity()
+        )
+        self.dropout = (
+            torch.nn.Dropout(dropout) if dropout > 0 else torch.nn.Identity()
+        )
 
     def __repr__(self):
         return f"{self.__class__.__name__}(in_channels={self.linear1.in_features}, out_channels={self.linear1.out_features})"
@@ -137,5 +150,7 @@ class SimpleEncoder(torch.nn.Module):
         """
 
         x = self.linear1(x)
-        # x = self.BN(x, batch=batch) if batch.shape[0] > 0 else self.BN(x)
+        if self.batch_norm:
+            x = self.BN(x, batch=batch) if batch.shape[0] > 0 else self.BN(x)
+        x = self.dropout(x)
         return x

topobenchmarkx/nn/wrappers/simplicial/sann_wrapper.py
@@ -1,7 +1,5 @@
 """Wrapper for the SANN model."""
 
-import torch
-
 from topobenchmarkx.nn.wrappers import AbstractWrapper
 
 
@@ -60,7 +58,7 @@ class SANNWrapper(AbstractWrapper):
         x_out = self.backbone(x_all)
 
         model_out = {
-            "labels": batch.y.type(torch.long),
+            "labels": batch.y,
             "batch_0": batch.batch_0,
             "batch_1": batch.batch_1,
             "batch_2": batch.batch_2,

topobenchmarkx/transforms/data_manipulations/precompute_khop_features.py
@@ -72,7 +72,9 @@ class PrecomputeKHopFeatures(torch_geometric.transforms.BaseTransform):
         # Create a dictionary that stores the i-simplices, the
         # j-th hop features matrix
         x_all = {
-            f"x{i}_{j}": torch.ones(1) for i in range(K) for j in range(T + 1)
+            f"x{i}_{j}": torch.ones(1).float()
+            for i in range(K)
+            for j in range(T + 1)
         }
 
         # TODO Sometimes te normalizations results in 0
@@ -124,7 +126,7 @@ class PrecomputeKHopFeatures(torch_geometric.transforms.BaseTransform):
 
         # Set the information for the 0-hop embeddings
         for i in range(K):
-            x_all[f"x{i}_0"] = data[f"x_{i}"]
+            x_all[f"x{i}_0"] = data[f"x_{i}"].float()
 
         # For each hop t=1,...,T
         for t in range(1, T + 1):
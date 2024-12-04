088e1c860c6a15ec60cd0fd53a203bb782d32262
Guillermo Bernárdez <guille_gbg@hotmail.com>
2024-12-04 13:44:45 -0800

    Fix bug

--- test/evaluator/test_evaluator.py
+++ test/evaluator/test_evaluator.py
@@ -3,10 +3,13 @@ import pytest
 
 from topobenchmark.evaluator import TBEvaluator
 
-def test_evaluator():
-    """ Setup the test."""
-    evaluator_multilable = TBEvaluator(task="multilabel classification")
-    evaluator_regression = TBEvaluator(task="regression")
-    with pytest.raises(ValueError):
-        TBEvaluator(task="wrong")
-    repr = evaluator_multilable.__repr__()
+class TestTBEvaluator:
+    """ Test the TBXEvaluator class."""
+
+    def setup_method(self):
+        """ Setup the test."""
+        self.evaluator_multilable = TBEvaluator(task="multilabel classification")
+        self.evaluator_regression = TBEvaluator(task="regression")
+        with pytest.raises(ValueError):
+            TBEvaluator(task="wrong")
+        repr = self.evaluator_multilable.__repr__()

03493c4fb2ed20754170fd637d6d889af9870c0c
Guillermo Bernárdez <guille_gbg@hotmail.com>
2024-12-04 12:49:21 -0800

    Check influence of .gitattributes

---
+++ test/evaluator/test_evaluator.py
@@ -0,0 +1,12 @@
+""" Test the TBEvaluator class."""
+import pytest
+
+from topobenchmark.evaluator import TBEvaluator
+
+def test_evaluator(self):
+    """ Setup the test."""
+    evaluator_multilable = TBEvaluator(task="multilabel classification")
+    evaluator_regression = TBEvaluator(task="regression")
+    with pytest.raises(ValueError):
+        TBEvaluator(task="wrong")
+    repr = evaluator_multilable.__repr__()

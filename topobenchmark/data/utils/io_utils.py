37f5a1530434af7c0171cd1de15170fc64d5d8d3
Guillermo Bernárdez <guille_gbg@hotmail.com>
2024-12-04 16:57:07 -0800

    Revert "Update Docs and Codecov"
    This reverts commit bbc8ac8a6ab898a666bc8a77cd427ed4085d61f9.

--- topobenchmark/data/utils/io_utils.py
+++ topobenchmark/data/utils/io_utils.py
@@ -13,6 +13,7 @@ from torch_geometric.data import Data
 from torch_sparse import coalesce
 
 
+# Function to extract file ID from Google Drive URL
 def get_file_id_from_url(url):
     """Extract the file ID from a Google Drive file URL.
 
@@ -46,6 +47,7 @@ def get_file_id_from_url(url):
     return file_id
 
 
+# Function to download file from Google Drive
 def download_file_from_drive(
     file_link, path_to_save, dataset_name, file_format="tar.gz"
 ):

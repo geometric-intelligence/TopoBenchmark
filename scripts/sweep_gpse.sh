7763b0688af2902c7b50380aed15b0f9b7a49208
Martin Carrasco <martin.carrasco@utec.edu.pe>
2024-12-24 00:00:04 -0500

    FIX: Correct directoy for files

--- scripts/sweep_gpse.sh
+++ scripts/sweep_gpse.sh
@@ -2,18 +2,18 @@
 if [ ! -d "data/pretrained_models" ]; then
     mkdir data/pretrained_models
 fi
-if [ ! -f "pretrained_models/gpse_zinc.pt" ]; then
+if [ ! -f "data/pretrained_models/gpse_zinc.pt" ]; then
     wget https://zenodo.org/record/8145095/files/gpse_model_zinc_1.0.pt -O data/pretrained_models/gpse_zinc.pt
 fi
-if [ ! -f "pretrained_models/gpse_geom.pt" ]; then
+if [ ! -f "data/pretrained_models/gpse_geom.pt" ]; then
     wget https://zenodo.org/record/8145095/files/gpse_model_geom_1.0.pt -O data/pretrained_models/gpse_geom.pt
 fi
 
-if [ ! -f "pretrained_models/gpse_molpcba.pt" ]; then
+if [ ! -f "data/pretrained_models/gpse_molpcba.pt" ]; then
     wget https://zenodo.org/record/8145095/files/gpse_model_molpcba_1.0.pt -O data/pretrained_models/gpse_molpcba.pt
 fi
 
-if [ ! -f "pretrained_models/gpse_pcqm4mv2.pt" ]; then
+if [ ! -f "data/pretrained_models/gpse_pcqm4mv2.pt" ]; then
     wget https://zenodo.org/record/8145095/files/gpse_model_pcqm4mv2_1.0.pt -O data/pretrained_models/gpse_pcqm4mv2.pt
 fi
 datasets=('ZINC')

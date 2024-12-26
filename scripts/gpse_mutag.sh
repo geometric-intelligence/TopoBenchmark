0da6c0a8b89f85364c0f4b6729c8a2d3ba6026e4
Martin Carrasco <martin.carrasco@utec.edu.pe>
2024-12-26 00:26:11 -0500

    FIX: GPSE mutag

--- scripts/gpse_mutag.sh
+++ scripts/gpse_mutag.sh
@@ -10,19 +10,22 @@ do
         model=simplicial/sann\
         model.backbone.n_layers=1,2,3,4\
         model.feature_encoder.proj_dropout=0.25\
+        model.feature_encoder.out_channels=64,128\
         dataset.split_params.data_seed=0,1,2,4\
         dataset.dataloader_params.batch_size=128,256\
-        model.feature_encoder.out_channels=64,128\
-        transforms.sann_encoding.pretrain_model=$pretrain_model\
+        +dataset.parameters.max_node_degree=10 \
         optimizer.parameters.weight_decay=0,0.0001\
         optimizer.parameters.lr=0.01,0.001\
         trainer.max_epochs=500\
         trainer.min_epochs=50\
         trainer.devices=\[$CUDA\]\
-        optimizer.scheduler=null\
         trainer.check_val_every_n_epoch=5\
+        optimizer.scheduler=null\
         callbacks.early_stopping.patience=10\
+        +transforms/data_manipulations@transforms.node_deg=node_degrees \
+        +transforms/data_manipulations@transforms.node_feat=one_hot_node_degree_features \
         transforms/data_manipulations@transforms.sann_encoding=add_gpse_information\
+        transforms.sann_encoding.pretrain_model=$pretrain_model\
         logger.wandb.project=$project_name\
         transforms.sann_encoding.neighborhoods='[incidence_1,incidence_0]','[incidence_0]','[incidence_1,0_incidence_1,incidence_0]'\
         --multirun &

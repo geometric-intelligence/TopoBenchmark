1392d47f7facd7b547d08fc8cdae8981dd38afc2
Martin Carrasco <martin.carrasco@utec.edu.pe>
2025-01-01 16:04:21 -0500

    ADD: Scripts for executing random baseline

---
+++ scripts/random_proteins.sh
@@ -0,0 +1,29 @@
+dataset='PROTEINS'
+project_name="TBX_RAND_$dataset"
+CUDA=3
+
+seeds=(0 1 2 4)
+for seed in ${seeds[*]}
+do
+python topobenchmarkx/run.py\
+    dataset=graph/$dataset\
+    model=simplicial/sann\
+    model.backbone.n_layers=1,2,3,4\
+    model.feature_encoder.proj_dropout=0.25\
+    dataset.split_params.data_seed=$seed\
+    dataset.dataloader_params.batch_size=128,256\
+    model.feature_encoder.out_channels=64,128\
+    transforms/data_manipulations@transforms.sann_encoding=different_gaus_features_sann \
+    optimizer.parameters.weight_decay=0,0.0001\
+    optimizer.parameters.lr=0.01,0.001\
+    trainer.max_epochs=500\
+    trainer.min_epochs=50\
+    trainer.devices=\[$CUDA\]\
+    optimizer.scheduler=null\
+    trainer.check_val_every_n_epoch=5\
+    callbacks.early_stopping.patience=10\
+    logger.wandb.project=$project_name\
+    --multirun &
+done
+
+# transforms.sann_encoding.neighborhoods='[incidence_1,incidence_0]','[incidence_0]','[incidence_1,0_incidence_1,incidence_0]'\

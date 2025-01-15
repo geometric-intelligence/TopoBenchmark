e4165d81f83612dcee230f382ac8c9b80a806564
Luís F. Pereira <luisfgper@gmail.com>
2025-01-14 16:05:29 -0800

    Update SimplicialKHopLifting to work with new design

--- topobenchmark/transforms/liftings/graph2simplicial/khop.py
+++ topobenchmark/transforms/liftings/graph2simplicial/khop.py
@@ -4,15 +4,14 @@ import random
 from itertools import combinations
 from typing import Any
 
+import torch
 import torch_geometric
 from toponetx.classes import SimplicialComplex
 
-from topobenchmark.transforms.liftings.graph2simplicial.base import (
-    Graph2SimplicialLifting,
-)
+from topobenchmark.transforms.liftings.base import LiftingMap
 
 
-class SimplicialKHopLifting(Graph2SimplicialLifting):
+class SimplicialKHopLifting(LiftingMap):
     r"""Lift graphs to simplicial complex domain.
 
     The function lifts a graph to a simplicial complex by considering k-hop
@@ -23,38 +22,43 @@ class SimplicialKHopLifting(Graph2SimplicialLifting):
 
     Parameters
     ----------
+    complex_dim : int
+        Dimension of the desired complex.
     max_k_simplices : int, optional
         The maximum number of k-simplices to consider. Default is 5000.
-    **kwargs : optional
-        Additional arguments for the class.
     """
 
-    def __init__(self, max_k_simplices=5000, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, complex_dim=3, max_k_simplices=5000):
+        super().__init__()
+        self.complex_dim = complex_dim
         self.max_k_simplices = max_k_simplices
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(max_k_simplices={self.max_k_simplices!r})"
 
-    def lift_topology(self, data: torch_geometric.data.Data) -> dict:
+    def lift(self, domain):
         r"""Lift the topology to simplicial complex domain.
 
         Parameters
         ----------
-        data : torch_geometric.data.Data
-            The input data to be lifted.
+        domain : nx.Graph
+            Graph to be lifted.
 
         Returns
         -------
-        dict
-            The lifted topology.
+        toponetx.Complex
+            Lifted simplicial complex.
         """
-        graph = self._generate_graph_from_data(data)
+        graph = domain
+
         simplicial_complex = SimplicialComplex(graph)
-        edge_index = torch_geometric.utils.to_undirected(data.edge_index)
+        edge_index = torch_geometric.utils.to_undirected(
+            torch.tensor(list(zip(*graph.edges, strict=False)))
+        )
         simplices: list[set[tuple[Any, ...]]] = [
             set() for _ in range(2, self.complex_dim + 1)
         ]
+
         for n in range(graph.number_of_nodes()):
             # Find 1-hop node n neighbors
             neighbors, _, _, _ = torch_geometric.utils.k_hop_subgraph(
@@ -67,10 +71,12 @@ class SimplicialKHopLifting(Graph2SimplicialLifting):
             for i in range(1, self.complex_dim):
                 for c in combinations(neighbors, i + 1):
                     simplices[i - 1].add(tuple(c))
+
         for set_k_simplices in simplices:
             list_k_simplices = list(set_k_simplices)
             if len(set_k_simplices) > self.max_k_simplices:
                 random.shuffle(list_k_simplices)
                 list_k_simplices = list_k_simplices[: self.max_k_simplices]
             simplicial_complex.add_simplices_from(list_k_simplices)
-        return self._get_lifted_topology(simplicial_complex, graph)
+
+        return simplicial_complex

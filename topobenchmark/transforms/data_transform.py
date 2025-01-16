f4010fad4856a3423d59354ea4a2adf31a455d8f
Luís F. Pereira <luisfgper@gmail.com>
2025-01-16 15:31:50 -0800

    Update data_transform to handle new liftings design

--- topobenchmark/transforms/data_transform.py
+++ topobenchmark/transforms/data_transform.py
@@ -1,8 +1,54 @@
 """DataTransform class."""
 
+import inspect
+
 import torch_geometric
 
-from topobenchmark.transforms import TRANSFORMS
+from topobenchmark.transforms import LIFTINGS, TRANSFORMS
+from topobenchmark.transforms.liftings import (
+    GRAPH2CELL_LIFTINGS,
+    GRAPH2HYPERGRAPH_LIFTINGS,
+    GRAPH2SIMPLICIAL_LIFTINGS,
+    Graph2CellLiftingTransform,
+    Graph2HypergraphLiftingTransform,
+    Graph2SimplicialLiftingTransform,
+    LiftingTransform,
+)
+
+_map_lifting_types = {
+    "graph2cell": (GRAPH2CELL_LIFTINGS, Graph2CellLiftingTransform),
+    "graph2hypergraph": (
+        GRAPH2HYPERGRAPH_LIFTINGS,
+        Graph2HypergraphLiftingTransform,
+    ),
+    "graph2simplicial": (
+        GRAPH2SIMPLICIAL_LIFTINGS,
+        Graph2SimplicialLiftingTransform,
+    ),
+}
+
+
+def _map_lifting_name(lifting_name):
+    for liftings_dict, Transform in _map_lifting_types.values():
+        if lifting_name in liftings_dict:
+            return Transform
+
+    return LiftingTransform
+
+
+def _route_lifting_kwargs(kwargs, LiftingMap):
+    lifting_map_sign = inspect.signature(LiftingMap)
+
+    lifting_map_kwargs = {}
+    transform_kwargs = {}
+
+    for key, value in kwargs.items():
+        if key in lifting_map_sign.parameters:
+            lifting_map_kwargs[key] = value
+        else:
+            transform_kwargs[key] = value
+
+    return lifting_map_kwargs, transform_kwargs
 
 
 class DataTransform(torch_geometric.transforms.BaseTransform):
@@ -19,14 +65,21 @@ class DataTransform(torch_geometric.transforms.BaseTransform):
     def __init__(self, transform_name, **kwargs):
         super().__init__()
 
-        kwargs["transform_name"] = transform_name
-        self.parameters = kwargs
+        if transform_name not in LIFTINGS:
+            kwargs["transform_name"] = transform_name
+            transform = TRANSFORMS[transform_name](**kwargs)
+        else:
+            LiftingMap_ = TRANSFORMS[transform_name]
+            Transform = _map_lifting_name(transform_name)
+            lifting_map_kwargs, transform_kwargs = _route_lifting_kwargs(
+                kwargs, LiftingMap_
+            )
 
-        self.transform = (
-            TRANSFORMS[transform_name](**kwargs)
-            if transform_name is not None
-            else None
-        )
+            lifting_map = LiftingMap_(**lifting_map_kwargs)
+            transform = Transform(lifting_map, **transform_kwargs)
+
+        self.parameters = kwargs
+        self.transform = transform
 
     def forward(
         self, data: torch_geometric.data.Data
